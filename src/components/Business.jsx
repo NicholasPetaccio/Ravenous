fc7d2a8550a781eba0eb9a75e005fe064683eb44
Nick <nicholaspetaccio@gmail.com>
2024-04-30 04:01:01 -0400

    added business component with static setup to simulate website functionality

---
+++ src/components/Business.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Business() {
+    const business = {
+        image: 'https://via.placeholder.com/500',
+        name: 'exampleName',
+        address: 'exampleAddress',
+        city: 'exampleCity',
+        state: 'exampleState',
+        zipcode: 'exampleZip',
+        category: 'exampleCat',
+        rating: '0/5',
+        reviewCount: '0' 
+    };
+};
+
+export default Business;
