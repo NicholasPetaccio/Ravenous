b64117dd9657a2337f229804b65d8735b2494a28
Nick <nicholaspetaccio@gmail.com>
2024-05-09 00:11:16 -0400

    added hooks to track button state as well as implemented active styles to add visual cues for user

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./SearchBar.css";
 
 const SearchBar = () => {
+  const [name, setName] = useState("");
+  const [activeSort, setActiveSort] = useState("Best Match");
+  const sortRefs = {
+    "Best Match": useRef(null),
+    "Highest Rated": useRef(null),
+    "Most Reviewed": useRef(null),
+  };
+
+  const handleSortClick = (sortType) => {
+    setActiveSort(sortType);
+  };
+
+  const activeStyle = {
+    left: sortRefs[activeSort].current
+      ? sortRefs[activeSort].current.offsetLeft + "px"
+      : "0px",
+    width: sortRefs[activeSort].current
+      ? sortRefs[activeSort].current.offsetWidth + "px"
+      : "0px",
+  };
+
   return (
     <div className="searchForms">
       <div className="sortOptions">
-        <button className="sortBtn" type="submit">
-          Best Match
-        </button>
-        <button className="sortBtn" type="submit">
-          Highest Rated
-        </button>
-        <button className="sortBtn" type="submit">
-          Most Reviewed
-        </button>
+        {["Best Match", "Highest Rated", "Most Reviewed"].map((sortType) => (
+          <button
+            ref={sortRefs[sortType]}
+            className={`sortBtn ${activeSort === sortType ? "active" : ""}`}
+            onClick={() => handleSortClick(sortType)}
+            type="button"
+          >
+            {sortType}
+          </button>
+        ))}
+        <div className="underline" style={activeStyle}></div>
       </div>
       <div className="searchbars">
         <form>
@@ -21,6 +44,8 @@ const SearchBar = () => {
             type="text"
             placeholder="Search Business"
             name="search"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           ></input>
         </form>
         <input type="text" placeholder="Where?" name="where"></input>
